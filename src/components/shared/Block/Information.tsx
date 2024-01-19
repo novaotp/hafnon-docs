b7c29aac001f287271d20a9ef3e5bf4f8a9f3962
novaotp <rahmansajidur30@gmail.com>
2024-01-19 15:48:13 +0100

    Fix: removed rounding

--- src/components/shared/Block/Information.tsx
+++ src/components/shared/Block/Information.tsx
@@ -2,7 +2,7 @@ import { InfoRounded } from "@mui/icons-material"
 
 export const Information = ({ children }: { children: string }) => {
     return (
-        <div className="relative w-full flex flex-col p-5 justify-center items-start bg-gray-200 rounded-md">
+        <div className="relative w-full flex flex-col p-5 justify-center items-start bg-gray-200">
             <p className="font-bold mb-5"><InfoRounded className="fill-blue-600" /> Information</p>
             <p>{children}</p>
         </div>
