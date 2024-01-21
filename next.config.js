0533bd72a42a8354b5906a2b9ca7ccc44b6a1c27
novaotp <rahmansajidur30@gmail.com>
2024-01-21 11:30:35 +0100

    Fix: modified output in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'build',
+    output: 'export',
     basePath: '/hafnon-docs',
     images: {
         unoptimized: true
